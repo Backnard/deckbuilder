4adb7cc83bef8aa099b89da3ac56dd00e0ba80d1
Brian Backes <35049560+Backnard@users.noreply.github.com>
2020-06-13 19:22:52 -0700

    finished day 3!

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,15 +1,25 @@
 import React from "react";
-import './SearchResults.css';
 import Card from './Card';
+import './SearchResults.css';
 
-const SearchResults = ({ results }) => {
+const SearchResults = ({
+  results,
+  addCardToDeck,
+  removeCardFromDeck
+}) => {
   return (
     <div id="results">
       <h3>Here's what we found ({results.length} results):</h3>
       <div className="CardList">
-        {results.map((result) => (
-          <Card key={result.id} {...result} />
-        ))}
+        {
+        results.map(result => (
+          <Card
+          key={result.id}
+          addCardToDeck={addCardToDeck}
+          removeCardFromDeck={removeCardFromDeck}
+          {...result} />
+        ))
+        }
       </div>
     </div>
   );
